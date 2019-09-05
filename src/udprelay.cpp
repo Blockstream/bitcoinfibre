fe006b965caa4edc4d7e14d7ca8d6aae383e46b9
arvidn <arvid@libtorrent.org>
2019-09-05 13:56:39 +0200

    Wrap FEC Txn serialization with CTxCompressor
    - Introduce version number for compression scheme to allow smooth future
      upgrades.
    
    - Leave the prefilled transaction in the compact block header
      uncompressed (which in practice is only the coinbase transaction).

--- src/udprelay.cpp
+++ src/udprelay.cpp
@@ -337,7 +337,7 @@ void UDPRelayBlock(const CBlock& block) {
             initd = std::chrono::steady_clock::now();
 
         boost::optional<ChunkCodedBlock> codedBlock;
-        CBlockHeaderAndLengthShortTxIDs headerAndIDs(block, true);
+        CBlockHeaderAndLengthShortTxIDs headerAndIDs(block, codec_version_t::default_version, true);
         std::vector<unsigned char> header_data;
         header_data.reserve(2500 + 8 * block.vtx.size()); // Rather conservatively high estimate
         VectorOutputStream stream(&header_data, SER_NETWORK, PROTOCOL_VERSION);
@@ -468,7 +468,10 @@ void UDPFillMessagesFromTx(const CTransaction& tx, std::vector<std::pair<UDPMess
 
     std::vector<unsigned char> data;
     VectorOutputStream stream(&data, SER_NETWORK, PROTOCOL_VERSION);
-    stream << tx;
+
+    codec_version_t const codec_version = codec_version_t::default_version;
+    stream << static_cast<std::uint8_t>(codec_version);
+    stream << CTxCompressor(tx, codec_version);
 
     const size_t data_chunks = DIV_CEIL(data.size(), FEC_CHUNK_SIZE);
     msgs.resize(data_chunks);
@@ -538,7 +541,7 @@ void UDPFillMessagesFromBlock(const CBlock& block, std::vector<UDPMessage>& msgs
     const uint64_t hash_prefix = hashBlock.GetUint64(0);
 
     /* FIBRE block header */
-    CBlockHeaderAndLengthShortTxIDs headerAndIDs(block, true);
+    CBlockHeaderAndLengthShortTxIDs headerAndIDs(block, codec_version_t::default_version, true);
 
     std::vector<unsigned char> header_data;
     header_data.reserve(2500 + 8 * block.vtx.size()); // Rather conservatively high estimate
@@ -959,13 +962,15 @@ static bool HandleTx(UDPMessage& msg, size_t length, const CService& node, UDPCo
 
         try {
             VectorInputStream stream(&tx_data, SER_NETWORK, PROTOCOL_VERSION);
+            codec_version_t codec_version;
+            stream >> *reinterpret_cast<std::uint8_t*>(&codec_version);
             CTransactionRef tx;
-            stream >> tx;
+            stream >> CTxCompressor(tx, codec_version);
             LOCK(cs_main);
             CValidationState state;
             AcceptToMemoryPool(mempool, state, tx, nullptr, nullptr, false, 0);
-        } catch (std::ios_base::failure& e) {
-            LogPrintf("UDP: Tx decode failed for tx %lu from %s\n", msg.msg.block.hash_prefix, node.ToString());
+        } catch (std::exception& e) {
+            LogPrintf("UDP: Tx decode failed for tx %lu from %s: %s\n", msg.msg.block.hash_prefix, node.ToString(), e.what());
         }
 
         state.tx_in_flight.reset();
