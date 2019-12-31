bc57159b913e0ca655b63a06385b66f0b0f09ec6
Arvid Norberg <arvid@blockstream.io>
2019-12-31 15:40:22 +0100

    Remove outdated mutex annotation for StoreOoOBlock
    cs_main is locked inside the call now.

--- src/validation.h
+++ src/validation.h
@@ -96,7 +96,7 @@ extern uint256 g_best_block;
 /** Documentation for argument 'checklevel'. */
 extern const std::vector<std::string> CHECKLEVEL_DOC;
 
-bool StoreOoOBlock(const CChainParams&, const std::shared_ptr<const CBlock>) EXCLUSIVE_LOCKS_REQUIRED(cs_main);
+bool StoreOoOBlock(const CChainParams&, const std::shared_ptr<const CBlock>);
 void ProcessSuccessorOoOBlocks(const CChainParams&, const uint256& prev_block_hash);
 void CheckForOoOBlocks(const CChainParams&);
 
