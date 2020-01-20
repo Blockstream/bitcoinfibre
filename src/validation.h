f89f8ff4c3383bd29a1bfa006cc446f001950e51
Blockstream Satellite <satellite@blockstream.com>
2020-01-20 13:58:31 -0300

    Add option to force of out-of-order block storage
    Even if the block is far in the future, with this option it would be
    stored. This allows more efficient full chain sync entirely via UDP delivery.
    
    Relates to Issue !5

--- src/validation.h
+++ src/validation.h
@@ -96,7 +96,7 @@ extern uint256 g_best_block;
 /** Documentation for argument 'checklevel'. */
 extern const std::vector<std::string> CHECKLEVEL_DOC;
 
-bool StoreOoOBlock(const CChainParams&, const std::shared_ptr<const CBlock>);
+bool StoreOoOBlock(const CChainParams&, const std::shared_ptr<const CBlock>, bool force);
 void ProcessSuccessorOoOBlocks(const CChainParams&, const uint256& prev_block_hash);
 void CheckForOoOBlocks(const CChainParams&);
 
