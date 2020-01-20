034e8f4cdffdbe9c61522bed9d5c409e7bd69959
Blockstream Satellite <satellite@blockstream.com>
2020-01-20 15:08:41 -0300

    Send height of backfill blocks to aid OOOB storage
    Includes a field for the block height on CBlockHeaderAndLengthShortTxIDs and
    PartiallyDownloadedChunkBlock structures. The sender can optionally fill the
    height on CBlockHeaderAndLengthShortTxIDs and the recipient will set the height
    in PartiallyDownloadedChunkBlock according to what it receives from an incoming
    CBlockHeaderAndLengthShortTxIDs.
    
    This is optional is meant to support storage of out-of-order blocks (OOOBs),
    specifically for pre-BIP34 blocks. The reason is that pre-BIP34 blocks don't
    include the height as part of the coinbase transaction's input script, and so
    unless the sender advertises the height info explicitly, the receive node won't
    know the height and won't be able to store the block in case it is received
    out-of-order.

--- src/blockencodings.h
+++ src/blockencodings.h
@@ -176,6 +176,7 @@ private:
     codec_version_t codec_version; // Compression/decompression scheme's version
     std::vector<uint32_t> txlens; // size by CTxCompressor
     friend class PartiallyDownloadedChunkBlock;
+    int height = -1; // Block height - for OOOB storage of pre-BIP34 blocks
 public:
 
     codec_version_t codec_ver() const { return codec_version; }
@@ -185,6 +186,8 @@ public:
 
     // Dummy for deserialization
     CBlockHeaderAndLengthShortTxIDs() {}
+
+    void setBlockHeight(int h) { height = h; }
 
     // Fills a map from offset within a FEC-coded block to the tx index in the block
     // Returns false if this object is invalid (txlens.size() != shortxids.size())
@@ -196,6 +199,7 @@ public:
     template <typename Stream, typename Operation>
     inline void SerializationOp(Stream& s, Operation ser_action) {
         READWRITE(*reinterpret_cast<std::uint8_t*>(&codec_version));
+        READWRITE(height);
 
         READWRITE(*(CBlockHeaderAndShortTxIDs*)this);
         if (ser_action.ForRead()) {
@@ -236,8 +240,9 @@ private:
     bool block_finalized = false;
     std::shared_ptr<CBlock> decoded_block;
 
-    // this is initialied to what we read off the network in InitData()
+    // this is initialized to what we read off the network in InitData()
     codec_version_t codec_version = codec_version_t::default_version;
+    int height = -1; // either -1 or the block height advertised by the peer
 
     // Things used in the iterative fill-from-mempool:
     std::map<size_t, size_t>::iterator fill_coding_index_offsets_it;
@@ -271,6 +276,8 @@ public:
     // but can happen after MarkChunkAvailable
     unsigned char* GetChunk(size_t chunk);
     void MarkChunkAvailable(size_t chunk);
+
+    int getBlockHeight() { return height; }
 };
 
 #endif // BITCOIN_BLOCKENCODINGS_H
