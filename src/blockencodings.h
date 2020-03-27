34685a89f85eb5368a1b217e0cee9a1376c7940a
Blockstream Satellite <satellite@blockstream.com>
2020-03-27 11:00:16 -0300

    Support specific/all heights on getchunkstats RPC
    - Add option to get chunk stats for a given height
    - Add option to get chunk stats from all current partial blocks

--- src/blockencodings.h
+++ src/blockencodings.h
@@ -282,7 +282,7 @@ public:
     unsigned char* GetChunk(size_t chunk);
     void MarkChunkAvailable(size_t chunk);
 
-    int getBlockHeight() { return height; }
+    int getBlockHeight() const { return height; };
 };
 
 #endif // BITCOIN_BLOCKENCODINGS_H
