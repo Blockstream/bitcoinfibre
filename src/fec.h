125df44e36eb84d732916e1f1397b4e5622b24ba
Blockstream Satellite <satellite@blockstream.com>
2020-12-11 16:55:32 -0300

    Support FEC decoding in memory and mmap modes
    The first approach to FEC decoding loaded all FEC chunks in memory, which lead
    to excessive memory usage when processing thousands of blocks in parallel. More
    recently, it was changed to the second approach (existing before this patch),
    which loaded all FEC chunks on a memory-mapped file instead. This patch
    introduces a hybrid approach.
    
    In the hybrid approach, new blocks at the tip of the blockchain are placed
    directly in memory. These blocks are expected to be processed quickly so they
    are not expected to live in memory for too long. In contrast, old (historic)
    Bitcoin blocks are decoded in mmap mode. These come in parallel and, thus, mmap
    mode is used to avoid excessive memory usage through disk offloading.
    
    The hybrid approach allows for an optimized processing time. The blocks that
    really need to be processed quickly (new blockchain blocks) are processed in
    memory mode, which runs faster. Meanwhile, historic blocks take slightly longer
    to be processed based on the slow mmap approach, which is considered acceptable.
    
    This patch also changes a few aspects of the implementation, listed below:
    
    - In mmap mode, both chunks and chunk ids are now stored on the mmap
      file (offloaded to disk). The previous implementation stored only chunks and
      maintained chunk ids in memory (on an std::vector<uint32_t>). The new
      implementation is more memory-efficient by saving the vector of uint32 chunk
      ids into the mmmap file. This is useful to allow an even higher number of
      historic blocks received in parallel with acceptable memory utilization.
    
    - The former map_storage struct used to handle the writing and reading from the
      mmap file was extended and changed into a class named MapStorage. This class
      has a few new extra helper methods, such as handling of chunk ids, insertion
      of chunks on the mmap file, and the creation of the mmap file.
    
    - The cm256 decoding was moved from method GetDataPtr() to method
      ProvideChunk(). In other words, as soon as the last required cm256 chunk is
      provided, ProvideChunk will go ahead and decode the data. Then, when
      GetDataPtr is called, it only gets the decoded data directly.
    
      - In terms of udprelay functionality, this means that the cm256 decoding will
        happen inside HandleBlockTxMessage() instead of ProcessBlockThread(). This
        is considered acceptable. Aside from the decoding itself, the more expensive
        data copying (memcpy calls) still happens on GetDataPtr (i.e., from
        ProcessBlockThread in mmap mode.
    
    - The cm256 decoding now has two distinct implementations. One for memory mode
      and another for mmap mode. Similarly, ProvideChunk has two distinct
      implementations.
    
    - Unit tests were added to cover both memory and mmap modes of the FECDecoder.

--- src/fec.h
+++ src/fec.h
@@ -12,6 +12,7 @@
 #include <fs.h>
 
 #define FEC_CHUNK_SIZE 1152
+#define CHUNK_ID_SIZE sizeof(uint32_t)
 #define CM256_MAX_CHUNKS 27
 #define FEC_CHUNK_COUNT_MAX (1 << 24)
 
@@ -97,6 +98,33 @@ public:
     bool PrefillChunks();
 };
 
+enum class MemoryUsageMode : bool {
+    USE_MEMORY = false,
+    USE_MMAP = true
+};
+
+class MapStorage
+{
+public:
+    MapStorage(boost::filesystem::path const& p, int const c, bool create = false);
+    MapStorage(MapStorage&& ms) noexcept;
+
+    void Insert(const unsigned char* chunk, uint32_t chunk_id, size_t idx);
+    char* GetChunk(size_t idx) const;
+    uint32_t GetChunkId(size_t idx) const;
+    size_t Size() const;
+    char* GetStorage() const { return m_data_storage; }
+
+    ~MapStorage();
+
+private:
+    size_t m_chunk_count = 0;
+    int m_chunk_file = -1;
+    char* m_data_storage = nullptr;
+    char* m_id_storage = nullptr;
+    size_t m_file_size = 0;
+};
+
 class FECDecoder {
     FECChunkType tmp_chunk;
     size_t chunk_count = 0;
@@ -112,15 +140,17 @@ class FECDecoder {
     // destructed
     bool owns_file = false;
 
+    // Whether to store chunk ids and chunk data in memory (e.g. vector) or
+    // to store them in a memory mapped file on the disk
+    MemoryUsageMode memory_usage_mode = MemoryUsageMode::USE_MMAP;
+
     // maps final chunk ids to offset into the storage (backed by the file)
-    // filled in once cm256_decoded is set to true
     std::vector<uint8_t> cm256_map;
 
-    // the chunk IDs of the chunks stored in chunk_storage, in the order they
-    // are stored.
-    std::vector<uint32_t> chunk_ids;
-
     bool cm256_decoded = false;
+    // Only used in cm256 mode:
+    std::vector<FECChunkType> cm256_chunks;
+    cm256_block cm256_blocks[CM256_MAX_CHUNKS];
 
     void remove_file();
 
@@ -131,8 +161,19 @@ class FECDecoder {
 
     fs::path compute_filename() const;
 
+    bool ProvideChunkMemory(const unsigned char* chunk, uint32_t chunk_id);
+    bool ProvideChunkMmap(const unsigned char* chunk, uint32_t chunk_id);
+
+    void DecodeCm256();
+    void DecodeCm256Memory();
+    void DecodeCm256Mmap();
+
 public:
-    FECDecoder(size_t data_size); // data_size must be <= MAX_BLOCK_SERIALIZED_SIZE * MAX_CHUNK_CODED_BLOCK_SIZE_FACTOR
+    // data_size must be <= MAX_BLOCK_SERIALIZED_SIZE * MAX_CHUNK_CODED_BLOCK_SIZE_FACTOR
+    // memory_usage_mode if set to USE_MEM, all chunks and chunk ids are stored in a memory-mapped file on disk
+    //                  if set to USE_MEMORY, nothing is stored on disk and everything will live in the memory
+    FECDecoder(size_t data_size, MemoryUsageMode memory_usage_mode = MemoryUsageMode::USE_MMAP);
+
     FECDecoder();
     ~FECDecoder();
 
@@ -142,12 +183,14 @@ public:
 
     bool ProvideChunk(const unsigned char* chunk, uint32_t chunk_id);
     bool ProvideChunk(const FECChunkType* chunk, uint32_t chunk_id) { return ProvideChunk((const unsigned char*)chunk, chunk_id); }
+
     bool HasChunk(uint32_t chunk_id);
     bool DecodeReady() const;
     const void* GetDataPtr(uint32_t chunk_id); // Only valid until called again
     size_t GetChunkCount() const { return chunk_count; }
     size_t GetChunksRcvd() const { return chunks_recvd; }
     fs::path GetFileName() const { return filename; }
+
 };
 
 bool BuildFECChunks(const std::vector<unsigned char>& data, std::pair<std::unique_ptr<FECChunkType[]>, std::vector<uint32_t>>& fec_chunks);
