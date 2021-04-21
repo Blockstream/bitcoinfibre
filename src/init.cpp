6c73f5a1d3fa8ad587dc1aa2e630adfcfe8b26f3
Blockstream Satellite <satellite@blockstream.com>
2021-04-21 12:03:32 -0300

    Make UDP InitError bilingual to comply with v0.21
    InitError was made bilingual in Bitcoin Core v0.21.

--- src/init.cpp
+++ src/init.cpp
@@ -1795,7 +1795,7 @@ bool AppInitMain(NodeContext& node, interfaces::BlockAndHeaderTipInfo* tip_info)
 
     if (GetUDPInboundPorts().size() || gArgs.GetArg("-udpmulticast", "") != "" || gArgs.GetArg("-udpmulticasttx", "") != "") {
         if (!InitializeUDPConnections(node.connman.get()))
-            return InitError(_("Failed to initialize UDP connections").translated);
+            return InitError(_("Failed to initialize UDP connections"));
     }
 
 
