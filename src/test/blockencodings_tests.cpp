5ef28d61cb06f0e5ab8368d02fb4eaa7e9c747e2
Blockstream Satellite <satellite@blockstream.com>
2021-04-22 11:36:53 -0300

    Remove duplicate block413567 hex files
    They were introduced for FEC tests and related blockencodings
    tests. However, they are unnecessary given that there is already a raw
    binary block413567 available in the source tree and used for
    benchmarking data. Hence, simply reuse the available raw binary on FEC
    benchmarking and blockencodings tests.

--- src/test/blockencodings_tests.cpp
+++ src/test/blockencodings_tests.cpp
@@ -2,6 +2,7 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+#include <bench/data.h>
 #include <blockencodings.h>
 #include <chainparams.h>
 #include <consensus/merkle.h>
@@ -12,8 +13,6 @@
 #include <test/util/txmempool.h>
 
 #include <test/util/setup_common.h>
-
-#include "test/data/block413567.hex.h"
 
 #include <boost/test/unit_test.hpp>
 #include <random>
@@ -605,9 +604,7 @@ BOOST_AUTO_TEST_CASE(RealFECedBlockRoundTripTest)
 {
     CBlock block;
 
-    CDataStream stream((const char*)blockencodings_tests::block413567,
-            (const char*)&blockencodings_tests::block413567[sizeof(blockencodings_tests::block413567)],
-            SER_NETWORK, PROTOCOL_VERSION);
+    CDataStream stream(benchmark::data::block413567, SER_NETWORK, PROTOCOL_VERSION);
     stream >> block;
 
     bool mutated;
