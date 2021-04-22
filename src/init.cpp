ehsanen/bitcoinsatellite

udpnet: support multiplexed multicast Tx streams
Before this patch, a single multicast group would be allowed per (destination IP
address, network interface) pair. This patch allows multiple multiplexed
multicast Tx streams for the same destination address and through the same
interface.

Each multicast Tx stream now has a physical and a logical index. The physical
index is uniquely associated to a pair of (destination IPv4 address, network
interface index). The multicast tx streams that share the same physical index
will necessarily have different (unique) logical indexes.

Among the logical streams that share the same physical index, only one of them
will be deemed as the "main" stream. As of this patch, the decision rule for
this is based on throughput. The logical stream with highest throughput becomes
the main. The importance of this is that only the main logical stream is allowed
to send txns and also to send new blocks as they appear, i.e. to perform block
preemption and prioritise the transmission of a new block. The motivation is to
prevent all multiplexed streams to repeating the information of txns and new
blocks.

Also, it is noteworthy that this patch changes a previous restriction of a
single backfill thread instance. Now, a new thread is launched for each
multicast Tx stream defined as `udpmulticasttx` within `bitcoin.conf`. Instead
of using a globally-defined CService for multicast Tx, now the corresponding
CService is passed the backfill thread when launching it.

Furthermore, this patch also changes how tx queues are defined. They are now
defined within a map, where UDP group is the key, rather than a vector of
queues, as before. The previous approach (based on vector) would take the UDP
group index and assume that the corresponding Tx queue existed exactly in that
index within the vector of queues. The limitation, however, relates to the fact
that not all groups require Tx queues, more specifically multicast Rx instances
don't (they are inbound-on

src/init.cpp
@@ -442,7 +442,7 @@ void SetupServerArgs()
     gArgs.AddArg("-udpport=<port>,<group>[,<bw>]", "Accepts UDP connections on <port> (default: bw=1024 =1024Mbps)", ArgsManager::ALLOW_ANY, OptionsCategory::CONNECTION);
 
     gArgs.AddArg("-udpmulticast=<if>,<dst_ip>:<port>,<src_ip>[,<label>]", "Listen to multicast-addressed UDP messages sent by <src_ip> towards <dst_ip>:<port> using interface <if>. An optional <label> may be defined for the multicast group in order to facilitate inspection of logs.", ArgsManager::ALLOW_ANY, OptionsCategory::UDP);
-    gArgs.AddArg("-udpmulticasttx=<if>,<ip/host>:<port>,<bw>[,<ttl>]", "Transmit multicast-addressed mesages to <ip/host>:<port> through interface <if> with bandwidth <bw> and TTL <ttl> (3 by default).", ArgsManager::ALLOW_ANY, OptionsCategory::UDP);
+    gArgs.AddArg("-udpmulticasttx=<if>,<ip/host>:<port>,<bw>,<send_txns>[,<ttl>,<depth>]", "Transmit multicast-addressed mesages to <ip/host>:<port> through interface <if> with bandwidth <bw> and TTL <ttl> (3 by default). Iteratively transmit the past <depth> blocks. If <send_txns> is 1, send txns in addition to blocks.", ArgsManager::ALLOW_ANY, OptionsCategory::UDP);
     gArgs.AddArg("-udpmulticastloginterval=<interval>", "Change periodicity of multicast bit rate logs that are activated by option debug=udpmulticast. Set <interval> in seconds.", ArgsManager::ALLOW_ANY, OptionsCategory::UDP);
 #ifdef USE_UPNP
 #if USE_UPNP

Blockstream/bitcoinfibre

src/init.cpp
@@ -476,7 +476,7 @@ void SetupServerArgs(NodeContext& node)
     argsman.AddArg("-torpassword=<pass>", "Tor control port password (default: empty)", ArgsManager::ALLOW_ANY | ArgsManager::SENSITIVE, OptionsCategory::CONNECTION);
     argsman.AddArg("-udpport=<port>,<group>[,<bw>]", "Accepts UDP connections on <port> (default: bw=1024 =1024Mbps)", ArgsManager::ALLOW_ANY, OptionsCategory::CONNECTION);                   
     argsman.AddArg("-udpmulticast=<if>,<dst_ip>:<port>,<src_ip>[,<label>]", "Listen to multicast-addressed UDP messages sent by <src_ip> towards <dst_ip>:<port> using interface <if>. An optional <label> may be defined for the multicast group in order to facilitate inspection of logs.", ArgsManager::ALLOW_ANY, OptionsCategory::UDP);
-    argsman.AddArg("-udpmulticasttx=<if>,<ip/host>:<port>,<bw>[,<ttl>]", "Transmit multicast-addressed mesages to <ip/host>:<port> through interface <if> with bandwidth <bw> and TTL <ttl> (3 by default).", ArgsManager::ALLOW_ANY, OptionsCategory::UDP);
+    argsman.AddArg("-udpmulticasttx=<if>,<ip/host>:<port>,<bw>,<send_txns>[,<ttl>,<depth>]", "Transmit multicast-addressed mesages to <ip/host>:<port> through interface <if> with bandwidth <bw> and TTL <ttl> (3 by default). Iteratively transmit the past <depth> blocks. If <send_txns> is 1, send txns in addition to blocks.", ArgsManager::ALLOW_ANY, OptionsCategory::UDP);
     argsman.AddArg("-udpmulticastloginterval=<interval>", "Change periodicity of multicast bit rate logs that are activated by option debug=udpmulticast. Set <interval> in seconds.", ArgsManager::ALLOW_ANY, OptionsCategory::UDP);
 #ifdef USE_UPNP
 #if USE_UPNP