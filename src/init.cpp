ehsanen/bitcoinsatellite

Make min timeout used for chunk stats configurable
The minimum timeout choice depends on the underlying throughput of the UDP
multicast traffic. Hence, rather than changing the hard-coded timeout every time
we change throughput configurations, it is better to have it configurable.

Essentially, when the stats refer to an old (repeated) block A, and suddenly
comes a new block B that preempts transmission of old blocks (as it has higher
priority), at this point the chunks of block A will stop arriving until block B
finishes. If the duration of block B exceeds the average chunk arrival interval
of block A, then the logic would timeout and print the stats of block A, even
though it was not finished. To overcome this, we consider the minimum timeout,
which should cover at least the duration maximum-size new blocks. This duration,
in turn, depends on the underlying bit rate.

src/init.cpp
@@ -444,6 +444,7 @@ void SetupServerArgs()
     gArgs.AddArg("-udpmulticast=<if>,<dst_ip>:<port>,<src_ip>,<trusted>[,<label>]", "Listen to multicast-addressed UDP messages sent by <src_ip> towards <dst_ip>:<port> using interface <if>. Set <trusted> to 1 if sender is a trusted node. An optional <label> may be defined for the multicast group in order to facilitate inspection of logs.", ArgsManager::ALLOW_ANY, OptionsCategory::UDP);
     gArgs.AddArg("-udpmulticasttx=<if>,<ip/host>:<port>,<bw>,<txn_per_sec>[,<ttl>,<depth>,<dscp>,<interleave>]", "Transmit multicast-addressed mesages to <ip/host>:<port> through interface <if> with bandwidth <bw> in bps and TTL <ttl> (3 by default). Iteratively transmit the past <depth> blocks. If <depth> is set to 0, iterate over the full chain. If <txn_per_sec> is > 0, send this number of txns per second in addition to blocks. Optionally define a DSCP for marking IP packets. Interleave FEC chunks of blocks covering a window of <interleave> seconds. If <interleave> is set to 0, do not interleave FEC chunks.", ArgsManager::ALLOW_ANY, OptionsCategory::UDP);
     gArgs.AddArg("-udpmulticastloginterval=<interval>", "Change periodicity of multicast bit rate logs that are activated by option debug=udpmulticast. Set <interval> in seconds.", ArgsManager::ALLOW_ANY, OptionsCategory::UDP);
+    gArgs.AddArg("-udpfecstatstimeout=<timeout>", "Change timeout for printing FEC chunk stats of a partial block when using debug=fec. Set <timeout> in seconds.", ArgsManager::ALLOW_ANY, OptionsCategory::UDP);
 #ifdef USE_UPNP
 #if USE_UPNP
     gArgs.AddArg("-upnp", "Use UPnP to map the listening port (default: 1 when listening and no -proxy)", ArgsManager::ALLOW_ANY, OptionsCategory::CONNECTION);

Blockstream/bitcoinfibre

src/init.cpp
@@ -478,6 +478,7 @@ void SetupServerArgs(NodeContext& node)
     argsman.AddArg("-udpmulticast=<if>,<dst_ip>:<port>,<src_ip>,<trusted>[,<label>]", "Listen to multicast-addressed UDP messages sent by <src_ip> towards <dst_ip>:<port> using interface <if>. Set <trusted> to 1 if sender is a trusted node. An optional <label> may be defined for the multicast group in order to facilitate inspection of logs.", ArgsManager::ALLOW_ANY, OptionsCategory::UDP);
     argsman.AddArg("-udpmulticasttx=<if>,<ip/host>:<port>,<bw>,<txn_per_sec>[,<ttl>,<depth>,<dscp>,<interleave>]", "Transmit multicast-addressed mesages to <ip/host>:<port> through interface <if> with bandwidth <bw> in bps and TTL <ttl> (3 by default). Iteratively transmit the past <depth> blocks. If <depth> is set to 0, iterate over the full chain. If <txn_per_sec> is > 0, send this number of txns per second in addition to blocks. Optionally define a DSCP for marking IP packets. Interleave FEC chunks of blocks covering a window of <interleave> seconds. If <interleave> is set to 0, do not interleave FEC chunks.", ArgsManager::ALLOW_ANY, OptionsCategory::UDP);
     argsman.AddArg("-udpmulticastloginterval=<interval>", "Change periodicity of multicast bit rate logs that are activated by option debug=udpmulticast. Set <interval> in seconds.", ArgsManager::ALLOW_ANY, OptionsCategory::UDP);
+    argsman.AddArg("-udpfecstatstimeout=<timeout>", "Change timeout for printing FEC chunk stats of a partial block when using debug=fec. Set <timeout> in seconds.", ArgsManager::ALLOW_ANY, OptionsCategory::UDP);
 #ifdef USE_UPNP
 #if USE_UPNP
     argsman.AddArg("-upnp", "Use UPnP to map the listening port (default: 1 when listening and no -proxy)", ArgsManager::ALLOW_ANY, OptionsCategory::CONNECTION);