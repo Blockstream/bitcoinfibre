7c6aacd4fc005f96c7de7fc4936d3f80e55a75da
Blockstream Satellite <satellite@blockstream.com>
2021-08-24 18:52:46 -0300

    Fix outoforder.cpp including of validation.h
    Broken after applying clang-format on outoforder.cpp.

--- src/outoforder.h
+++ src/outoforder.h
@@ -1,6 +1,7 @@
 #ifndef BITCOIN_OUTOFORDER_H
 #define BITCOIN_OUTOFORDER_H
 
+#include <validation.h>
 
 bool StoreOoOBlock(const CChainParams&, const std::shared_ptr<const CBlock>, bool force, int in_height);
 void ProcessSuccessorOoOBlocks(ChainstateManager& chainman, const CChainParams&, const uint256& prev_block_hash, bool force = false);
