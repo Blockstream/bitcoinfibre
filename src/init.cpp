ehsanen/bitcoinsatellite

Interleave FEC chunks of consecutive blocks
1) Implementation

The previous multicast backfill implementation would send all chunks of the n-th
block, then all chunks of block n+1 and so on. The problem with this approach is
that any sudden outage (e.g. due to weather on the sat link) can cause the loss
of several chunks of the same block and eventually lead to failure on the FEC
decoding of the block. It is instead preferable to amortize the outage over
several blocks. For instance, it is better to miss 1 chunk of each of 100
blocks, rather than 100 chunks of the same block, as in the former case it may
still be possible to decode all blocks without the chunk that was lost from each
of them, provided that there is at least one chunk of overhead for each block.

This patch changes the backfill/multicast block transmission approach to support
the referred interleaving. The interleaving is specified as an argument of
`udpmulticasttx` configuration option and is given in terms of duration (in
minutes) that it takes to iterate over all blocks of the window while
transmiting a **single chunk** of each. This duration is set in minutes, and
internally the window of blocks is sized based on the capacity/bitrate of the
output link and the desired window duration. In the end, the window will contain
a fixed number blocks (because chunks are of fixed length) and as soon as a
block gets completely transmitted, a new block replaces it on the window.

The product `window_duration * chunk_overhead` should be specified to the
maximum expected outage interval. For example, for a maximum outage of 60 min,
the combination of 1-min window duration and 60 chunks of overhead would
work. Note that the larger the window duration, the longer it takes to transmit
a single block. For example, with a 1-min window, a block composed by 1000
chunks would take 1000 min to be fully transmitted (i.e. more than
16h!). Furthermore, the longer the window, the longer the Rx nodes will need to
maintain p

src/init.cpp
@@ -442,7 +442,7 @@ void SetupServerArgs()
     gArgs.AddArg("-udpport=<port>,<group>[,<bw>]", "Accepts UDP connections on <port> (default: bw=1024 =1024Mbps)", ArgsManager::ALLOW_ANY, OptionsCategory::CONNECTION);
 
     gArgs.AddArg("-udpmulticast=<if>,<dst_ip>:<port>,<src_ip>,<trusted>[,<label>]", "Listen to multicast-addressed UDP messages sent by <src_ip> towards <dst_ip>:<port> using interface <if>. Set <trusted> to 1 if sender is a trusted node. An optional <label> may be defined for the multicast group in order to facilitate inspection of logs.", ArgsManager::ALLOW_ANY, OptionsCategory::UDP);
-    gArgs.AddArg("-udpmulticasttx=<if>,<ip/host>:<port>,<bw>,<send_txns>[,<ttl>,<depth>,<dscp>]", "Transmit multicast-addressed mesages to <ip/host>:<port> through interface <if> with bandwidth <bw> and TTL <ttl> (3 by default). Iteratively transmit the past <depth> blocks. If <depth> is set to 0, iterate over the full chain. If <send_txns> is 1, send txns in addition to blocks. Optionally define a DSCP for marking IP packets.", ArgsManager::ALLOW_ANY, OptionsCategory::UDP);
+    gArgs.AddArg("-udpmulticasttx=<if>,<ip/host>:<port>,<bw>,<send_txns>[,<ttl>,<depth>,<dscp>,<interleave>]", "Transmit multicast-addressed mesages to <ip/host>:<port> through interface <if> with bandwidth <bw> in bps and TTL <ttl> (3 by default). Iteratively transmit the past <depth> blocks. If <depth> is set to 0, iterate over the full chain. If <send_txns> is 1, send txns in addition to blocks. Optionally define a DSCP for marking IP packets. Interleave FEC chunks of blocks covering a window of <interleave> minutes. If <interleave> is set to 0, do not interleave FEC chunks.", ArgsManager::ALLOW_ANY, OptionsCategory::UDP);
     gArgs.AddArg("-udpmulticastloginterval=<interval>", "Change periodicity of multicast bit rate logs that are activated by option debug=udpmulticast. Set <interval> in seconds.", ArgsManager::ALLOW_ANY, OptionsCategory::UDP);
 #ifdef USE_UPNP
 #if USE_UPNP

Blockstream/bitcoinfibre

src/init.cpp
@@ -508,7 +508,7 @@ void SetupServerArgs(ArgsManager& argsman)
     argsman.AddArg("-torpassword=<pass>", "Tor control port password (default: empty)", ArgsManager::ALLOW_ANY | ArgsManager::SENSITIVE, OptionsCategory::CONNECTION);
     argsman.AddArg("-udpport=<port>,<group>[,<bw>]", "Accepts UDP connections on <port> (default: bw=1024 =1024Mbps)", ArgsManager::ALLOW_ANY, OptionsCategory::CONNECTION);
     argsman.AddArg("-udpmulticast=<if>,<dst_ip>:<port>,<src_ip>,<trusted>[,<label>]", "Listen to multicast-addressed UDP messages sent by <src_ip> towards <dst_ip>:<port> using interface <if>. Set <trusted> to 1 if sender is a trusted node. An optional <label> may be defined for the multicast group in order to facilitate inspection of logs.", ArgsManager::ALLOW_ANY, OptionsCategory::UDP);
-    argsman.AddArg("-udpmulticasttx=<if>,<ip/host>:<port>,<bw>,<send_txns>[,<ttl>,<depth>,<dscp>]", "Transmit multicast-addressed mesages to <ip/host>:<port> through interface <if> with bandwidth <bw> and TTL <ttl> (3 by default). Iteratively transmit the past <depth> blocks. If <depth> is set to 0, iterate over the full chain. If <send_txns> is 1, send txns in addition to blocks. Optionally define a DSCP for marking IP packets.", ArgsManager::ALLOW_ANY, OptionsCategory::UDP);
+    argsman.AddArg("-udpmulticasttx=<if>,<ip/host>:<port>,<bw>,<send_txns>[,<ttl>,<depth>,<dscp>,<interleave>]", "Transmit multicast-addressed mesages to <ip/host>:<port> through interface <if> with bandwidth <bw> in bps and TTL <ttl> (3 by default). Iteratively transmit the past <depth> blocks. If <depth> is set to 0, iterate over the full chain. If <send_txns> is 1, send txns in addition to blocks. Optionally define a DSCP for marking IP packets. Interleave FEC chunks of blocks covering a window of <interleave> minutes. If <interleave> is set to 0, do not interleave FEC chunks.", ArgsManager::ALLOW_ANY, OptionsCategory::UDP);
     argsman.AddArg("-udpmulticastloginterval=<interval>", "Change periodicity of multicast bit rate logs that are activated by option debug=udpmulticast. Set <interval> in seconds.", ArgsManager::ALLOW_ANY, OptionsCategory::UDP);
 #ifdef USE_UPNP
 #if USE_UPNP